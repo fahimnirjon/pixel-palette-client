a9714fd066d1ef54ccdea879e921f0a5959e50ab
fahimnirjon <fahimnirjon21@gmail.com>
2024-09-09 22:45:34 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,99 @@
+import { createBrowserRouter } from "react-router-dom";
+import CategoryCard from "../components/CategoryCard";
+import CategoryDetails from "../components/CategoryDetails";
+import CeaftDetails from "../components/CeaftDetails";
+import UpdateProduct from "../components/UpdateProduct";
+import VeiwDetails from "../components/VeiwDetails";
+import AddArtsCarfts from "../pages/AddArtsCarfts";
+import AllArtsCarts from "../pages/AllArtsCarts";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import MyArtsCarfts from "../pages/MyArtsCarfts";
+import NotFoundPage from "../pages/NotFoundPage";
+import Register from "../pages/Register";
+import Private from "../Private/Private";
+import Root from "./Root";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/addartscrafts",
+        element: (
+          <Private>
+            <AddArtsCarfts />
+          </Private>
+        ),
+      },
+      {
+        path: "/allartscrafts",
+        element: <AllArtsCarts />,
+      },
+      {
+        path: "/myartscrafts",
+        element: (
+          <Private>
+            <MyArtsCarfts />
+          </Private>
+        ),
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/details/:id",
+        element: (
+          <Private>
+            <VeiwDetails></VeiwDetails>
+          </Private>
+        ),
+      },
+      {
+        path: "/update/:id",
+        element: (
+          <Private>
+            {" "}
+            <UpdateProduct></UpdateProduct>
+          </Private>
+        ),
+        loader: ({ params }) =>
+          fetch(`https://backend-orcin-ten-82.vercel.app/products/${params.id}`),
+      },
+      {
+        path: "/craftdetails/:id",
+        element: (
+          <Private>
+            <CeaftDetails></CeaftDetails>
+          </Private>
+        ),
+        loader: ({ params }) =>
+          fetch(`https://backend-orcin-ten-82.vercel.app/product/${params.id}`),
+      },
+      {
+        path: "/categorys/:category",
+        element: <CategoryCard></CategoryCard>,
+      },
+      {
+        path: "/categoryDetails/:id",
+        element: <CategoryDetails></CategoryDetails>,
+        loader: ({ params }) =>
+          fetch(
+            `https://backend-orcin-ten-82.vercel.app/categories/${params.id}`
+          ),
+      },
+    ],
+  },
+]);
+
+export default router;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./Routes/Routes";
+import AuthProvider from "./Provider/AuthProvider";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <Toaster />
+    </AuthProvider>
+  </React.StrictMode>
+);
